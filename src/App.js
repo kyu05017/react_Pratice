351bc9184beec84888adba82fd272fd1ec657bc4
kyu05017 <kyu05017@naver.com>
2022-07-04 12:52:56 +0900

    리액트 공부중

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import MC from './MyComponent';
+import MC from './practice/MyComponent';
+import EP from './practice/EventPractice';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <MC name="React" age="20" />
+                <EP />
+
             </div>
         );
     }
